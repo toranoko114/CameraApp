b50a0b37a232e1491d2244f6a5ed47037c9bbb47
toranoko114 <toranoko114.business@gmail.com>
2023-01-03 11:15:26 +0900

    Initial Commit

---
+++ CameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraApp
+//
+//  Created by 鈴木洋平 on 2023/01/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
